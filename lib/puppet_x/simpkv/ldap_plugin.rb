38e49711cab3cc40e88f64c6eca377c0096ab4f4
lnemsick-simp <lnemsick.simp@gmail.com>
2021-05-14 15:32:55 -0400

    more partial work

--- lib/puppet_x/simpkv/ldap_plugin.rb
+++ lib/puppet_x/simpkv/ldap_plugin.rb
@@ -4,6 +4,8 @@
 # a `plugin_class` local variable.
 plugin_class = Class.new do
   require 'facter'
+  require 'pathname'
+  require 'set'
 
   # Reminder:  Do **NOT** try to set constants in this Class.new block.
   #            They don't do what you expect (are not accessible within
@@ -49,7 +51,9 @@ plugin_class = Class.new do
   # * `tls_cert`:   Optional. Certificate file
   # * `tls_key`:    Optional. Key file
   # * `tls_cacert`: Optional. cacert file
-  # *
+  # * `retries`:    Optional. Number of times to retry an LDAP operation if the
+  #                 server reports it is busy.
+  #                 - Defaults to 1
   #
   # @param name Name to ascribe to this plugin instance
   # @param options Hash of global simpkv and backend-specific options
@@ -57,6 +61,12 @@ plugin_class = Class.new do
   #   FIXME other failures
   def initialize(name, options)
     @name = name
+
+    # Maintain list of folders that already exist to reduce the number of
+    # unnecessary ldap add operations over the lifetime of this plugin instance
+    @existing_folders = Set.new
+    @instance_path = File.join('instances', @name)
+    @search_opts= '-o ldif_wrap=no -LLL'
 
     # backend config should already have been verified by simpkv adapter, but
     # just in case...
@@ -77,10 +87,10 @@ plugin_class = Class.new do
 
     parse_config(options['backends'][options['backend']])
     verify_ldap_setup
+    ensure_instance_tree
 
     # create instance tree structure if it does not exist?
     #
-    @search_opts= '-o ldif_wrap=no -LLL'
 
     Puppet.debug("#{@name} simpkv plugin constructed")
   end
@@ -99,6 +109,7 @@ plugin_class = Class.new do
   #   * :err_msg - String. Explanatory text upon failure; nil otherwise.
   #
   def delete(key)
+    full_key_path =  File.join(@instance_path, key)
 
     # FIXME: insert code that connects to the backend an affects the delete
     # operation
@@ -120,6 +131,7 @@ plugin_class = Class.new do
   #   * :err_msg - String. Explanatory text upon failure; nil otherwise.
   #
   def deletetree(keydir)
+    full_keydir_path =  File.join(@instance_path, keydir)
 
     # FIXME: insert code that connects to the backend and affects the deletetree
     # operation
@@ -148,12 +160,13 @@ plugin_class = Class.new do
     scope = nil
     search_filter = ''
     if key.empty?
-      dn = base_dn
+      dn = @base_dn
       scope = '-s base'
     else
       # don't know if the key path is to a key or a folder so need to create a
       # search filter for both an ou=RDN or simpkvKey=RDN.
-      dn = path_to_dn(File.dirname(key), false)
+      full_key_path =  File.join(@instance_path, key)
+      dn = path_to_dn(File.dirname(full_key_path), false)
       leaf = File.basename(key)
       search_filter = "(|(ou=#{leaf})(simpkvKey=#{leaf}))"
       scope = '-s one'
@@ -163,30 +176,36 @@ plugin_class = Class.new do
     cmd = [
       @cmd_env,
       @ldapsearch,
-      @base_opts
+      @base_opts,
       '-b', dn,
-      @search_opts
+      @search_opts,
       scope,
       %Q{"#{search_filter}"},
       '1.1'                   # only print out the dn, no attributes
     ]
 
-    found = nil
+    found = false
     error_msg = nil
-    result = run_command(cmd.join)
-    if result[:exitstatus] == 0
-      # Parent DN exists, but search may or may not have returned a result.
-      # Have to parse console output to see if a dn was returned
-      if result[:stdout].match(%r{^dn: .*#{dn}})
-        found = true
+    done = false
+    retries = @retries
+    until done
+      result = run_command(cmd.join)
+      switch(result[:exitstatus])
+      case 0:
+        # Parent DN exists, but search may or may not have returned a result.
+        # Have to parse console output to see if a dn was returned.
+        found = true if result[:stdout].match(%r{^dn: .*#{dn}})
+        done = true
+      case 32:  # 'No such object'
+        # Some part of the parent DN does not exist
+        done = true
+      case 51: # 'Server is busy'
+        done = true if (retries == 0)
       else
-        found = false
+        error_msg = result[:stderr]
+        done = true
       end
-    elsif result[:exitstatus] == 32  # 'no such object'
-      # Some part of the parent DN does not exist
-      found = false
-    else
-      error_msg = result[:stderr]
+      retries -= 1
     end
 
     { :result => found, :err_msg => error_msg }
@@ -202,26 +221,46 @@ plugin_class = Class.new do
   #   * :err_msg - String. Explanatory text upon failure; nil otherwise.
   #
   def get(key)
+    full_key_path =  File.join(@instance_path, key)
 
-    # 1. construct DN
-    # 2. search
     # ldapsearch -x -w "P@ssw0rdP@ssw0rd" -D "cn=Directory_Manager" -H ldapi://%2fvar%2frun%2fslapd-simp_kv.socket -b "simpkvKey=key1,ou=production,ou=environments,ou=default,ou=simpkv,o=puppet,dc=simp" -o ldif_wrap=no -LLL
-    # 3. parse results
-    # if success
-    #    # will be successful even if search returns nothing
-    #    return key info or nil
-    # else
-    #    return failure info
-    # end
-    # FIXME: insert code that connects to the backend an affects the get
-    # operation
-    #
-    # - If possible, this get should be done atomically
-    # - Convert any exceptions into a failed status result with a meaningful
-    #   error message.
-    #
+    cmd = [
+      @cmd_env,
+      @ldapsearch,
+      @base_opts,
+      '-b', path_to_dn(full_key_path),
+      @search_opts
+    ]
 
-    { :result => nil, :err_msg => 'FIXME: not implemented' }
+    value = nil
+    err_msg = nil
+    done = false
+    retries = @retries
+    until done
+      result = run_command(cmd.join)
+      switch(result[:exitstatus])
+      case 0:
+          match = result[:stdout].match(/^simpkvJsonValue: (.*?)$/)
+          if match
+            value = match[1]
+          else
+            err_msg = "Key retrieval did not return key/value entry:"
+            err_msg += "\n#{result[:stdout]}"
+          end
+          done = true
+      case 51: # 'Server is busy'
+        if (retries == 0)
+          err_msg = result[:stderr]
+          done = true
+        end
+      else
+        err_msg = result[:stderr]
+        done = true
+      end
+      retries -= 1
+    end
+
+    { :result => value, :err_msg => err_msg }
   end
 
   # Returns a listing of all keys/info pairs and sub-folders in a folder
@@ -245,6 +284,25 @@ plugin_class = Class.new do
   #   * :err_msg - String. Explanatory text upon failure; nil otherwise.
   #
   def list(keydir)
+    full_keydir_path =  File.join(@instance_path, keydir)
+
+#ldapsearch -x -w "P@ssw0rdP@ssw0rd" -D "cn=Directory_Manager" -H ldapi://%2fvar%2frun%2fslapd-simp_kv.socket -b "ou=production,ou=environments,ou=default,ou=simpkv,o=puppet,dc=simp" -o ldif_wrap=no -LLL -s one
+#dn: simpkvKey=key1,ou=production,ou=environments,ou=default,ou=simpkv,o=puppet,dc=simp
+#objectClass: simpkvEntry
+#objectClass: top
+#simpkvKey: key1
+#simpkvJsonValue: {"value":"key1 value","metadata":{}}
+#
+#dn: ou=app1,ou=production,ou=environments,ou=default,ou=simpkv,o=puppet,dc=simp
+#ou: app1
+#objectClass: top
+#objectClass: organizationalUnit
+#
+#dn: ou=app2,ou=production,ou=environments,ou=default,ou=simpkv,o=puppet,dc=simp
+#ou: app2
+#objectClass: top
+#objectClass: organizationalUnit
+
 
     # use scope parameter in ldapsearch to ensure only going 1 level deep
     # FIXME: insert  code that connects to the backend an affects the list
@@ -267,19 +325,39 @@ plugin_class = Class.new do
   #   * :err_msg - String. Explanatory text upon failure; nil otherwise.
   #
   def put(key, value)
-    # at any point in this process, something else could be modifying 
-    # the database at the same time, so need to allow "Already exists" failures.
-    results = get(key)
-    if results[:result].nil?
-      results = create_entry(key, value)
-    else
-      if results[:result] != value
-        results = modify_entry(key, value)
-      else
-        # don't overwrite if the value is corect, because that
-        # unnecessarily changes the LDAP modify time
+    full_key_path =  File.join(@instance_path, key)
+
+    # We want to add the key/value entry if it does not exist but only modify
+    # the value if it does, so that we do not update modifyTimestamp
+    # unnecessarily. The tricky part is that at any point in this process,
+    # something else could be modifying the database at the same time. So
+    # there is no point in checking for the existence of the key's folders or
+    # its key/value entry, because that info may not be accurate at the time
+    # we request our changes.  Instead, try to add each node individually, and
+    # handle any "Already exists" failures appropriately for the node type.
+
+    results = nil
+    ldap_results = ensure_folder_path( File.dirname(full_key_path) )
+    if ldap_results[:success]
+      # first try an add for the key/value entry
+      ldif = entry_add_ldif(key, value)
+      ldap_results = ldap_add(ldif, false)
+
+      if ldap_results[:success]
         results = { :result => true, :err_msg => nil }
+      elsif (ldap_results[:exitstatus] == 68)  # Already exists
+        ldif = entry_modify_ldif(key, value)
+        ldap_results = ldap_modify(ldif, false)
+        if ldap_results[:success]
+          results = { :result => true, :err_msg => nil }
+        else
+          results = { :result => false, :err_msg => ldap_results[:err_msg] }
+        end
+      else
+        results = { :result => false, :err_msg => ldap_results[:err_msg] }
       end
+    else
+      results = { :result => false, :err_msg => ldap_results[:err_msg] }
     end
 
     results
@@ -287,60 +365,177 @@ plugin_class = Class.new do
 
   ###### Internal Methods ######
 
-  def create_entry(key, value)
-    folders_to_create = []
-    Pathname.new(File.dirname(key)).ascend do |folder|
-      break if exists(path)[:result]
-      folders_to_create << folder
+  # Ensures the basic tree for this instance is created below the base DN
+  #   base DN
+  #   | - instances
+  #   | | - <instance name>
+  #   | | | - globals
+  #   | | | - environments
+  #   | | --
+  #   | --
+  #   --
+  def ensure_instance_tree
+    [
+      File.join(@instance_path, 'globals'),
+      File.join(@instance_path, 'environments')
+    ].each do | folder|
+      # Have already verified access to the base DN, so going to *assume* any
+      # failures here are transient and will ignore them for now. If there is
+      # a persistent problem, it will be caught in the first key storage
+      # operation.
+      ensure_folder_path(folder)
+    end
+  end
+
+  # Ensure all folders in a folder path are present.
+  def ensure_folder_path(folder_path)
+    # Handle each folder separately instead of all at once, so we don't have to
+    # use log scraping to understand what happened...log scraping is fragile.
+    ldif_file = nil
+    folders_added = true
+    results = nil
+    Pathname.new(folder_path).descend do |folder|
+      next if @existing_folders.include?(folder)
+      ldif = folder_add_ldif(folder)
+      ldap_results = ldap_add(ldif, true)
+      if ldap_results[:success]
+        @existing_folders.add(folder)
+      else
+        folders_added = false
+        results = { :success => false, :exitcode => ldap_results[:exitcode], :err_msg => ldap_results[:err_msg] }
+        break
+      end
     end
 
-    ldif = ''
-    folders_to_create.reverse.each do |folder|
-      ldif += <<~EOM
-        ###########
-        dn: #{path_to_dn(folder, false)}
-        ou: #{File.basename(folder)}
-        objectClass: top
-        objectClass: organizationalUnit
+    results = { :success => true, :exitcode => 0, :err_msg => nil } if folders_added
+    results
+  end
 
-      EOM
+  def ldap_add(ldif, ignore_already_exists = false)
+    ldif_file = Tempfile.new('ldap_add')
+    ldif_file.puts(ldif)
+    ldif_file.close
+
+    # ldapadd -x -w "P@ssw0rdP@ssw0rd" -D "cn=Directory_Manager" -H ldapi://%2fvar%2frun%2fslapd-simp_kv.socket -f /root/simp_kv/ldifs/app2_group1_key1.ldif
+    cmd = [
+      @cmd_env,
+      @ldapadd,
+      @base_opts,
+      '-f', ldif_file.path
+    ]
+
+    added = false
+    exitstatus = nil
+    err_msg = nil
+    done = false
+    retries = @retries
+    until done
+      result = run_command(cmd.join)
+      switch(result[:exitstatus])
+      case 0:
+        added = true
+        done = true
+      case 68:  # Already exists
+        if ignore_already_exists
+          added = true
+        else
+          error_msg = result[:stderr]
+        end
+        done = true
+      case 51: # 'Server is busy'
+        if (retries == 0)
+          error_msg = result[:stderr]
+          done = true
+        end
+      else
+        error_msg = result[:stderr]
+        done = true
+      end
+      exitstatus = result[:exitstatus]
+      retries -= 1
     end
 
-    ldif += <<~EOM
-      ###########
+    { :success => added, :exitstatus => exitstatus, :err_msg => error_msg }
+  ensure
+    ldif_file.close if ldif_file
+    ldif_file.unlink if ldif_file
+  end
+
+  def ldap_modify(ldif)
+    ldif_file = Tempfile.new('ldap_add')
+    ldif_file.puts(ldif)
+    ldif_file.close
+
+    cmd = [
+      @cmd_env,
+      @ldapmodify,
+      @base_opts,
+      '-f', ldif_file.path
+    ]
+
+    modified = false
+    exitstatus = nil
+    error_msg = nil
+    done = false
+    retries = @retries
+    until done
+      result = run_command(cmd.join)
+      switch(result[:exitstatus])
+      case 0:
+        modified = true
+        done = true
+      case 32:  # 'No such object'
+        # DN got removed out from underneath us. Going to just accept this
+        # failure for now, as unclear the complication in the logic to turn
+        # around and add the entry is worth it.
+        error_msg = result[:stderr]
+        done = true
+      case 51: # 'Server is busy'
+        if (retries == 0)
+          error_msg = result[:stderr]
+          done = true
+        end
+      else
+        error_msg = result[:stderr]
+        done = true
+      end
+      exitstatus = result[:exitstatus]
+      retries -= 1
+    end
+
+    { :success => modified, :exitstatus => exitstatus, :err_msg => error_msg }
+  ensure
+    ldif_file.close if ldif_file
+    ldif_file.unlink if ldif_file
+
+  end
+
+  def folder_add_ldif(folder)
+    <<~EOM
+      dn: #{path_to_dn(folder, false)}
+      ou: #{File.basename(folder)}
+      objectClass: top
+      objectClass: organizationalUnit
+    EOM
+  end
+
+  def entry_add_ldif(key, value)
+    <<~EOM
       dn: #{path_to_dn(key)}
       objectClass: simpkvEntry
       objectClass: top
       simpkvKey: #{File.basename(key)}
       simpkvJsonValue: #{value}
     EOM
-
-    file = Tempfile.new('ldap_create_entry')
-    file.puts(ldif)
-    file.close
-# do we need the continuous -c option when we have folders to create
-# doesn't do what we want, returns the last failed return code
-# Need to iterate through each dir and ignore 68 failures (in case someone else is
-# creating dir at the same time)
-#
-# maybe better to run exists
-    cmd = [
-      @cmd_env,
-      @ldapadd,
-      @base_opts
-      '-b', dn,
-      @search_opts
-      scope,
-      %Q{"#{search_filter}"},
-      '1.1'                   # only print out the dn, no attributes
-    ]
-
-  ensure
-    file.close if file
-    file.unlink if file
   end
 
-  def modify_entry(key, value)
+  def entry_modify_ldif(key, value)
+    <<~EOM
+      dn: #{path_to_dn(key)}
+      changetype: modify
+      replace: simpkvJsonValue
+      simpkvJsonValue: #{value}
+    EOM
   end
 
   def path_to_dn(path, leaf_is_key = true)
@@ -350,8 +545,8 @@ plugin_class = Class.new do
       dn = @base_dn
     else
       attribute = leaf_is_key ? 'simpkvKey' : 'ou'
-      dn = "simpkvKey=#{parts.shift}"
-      parts.each do |folder|
+      dn = "#{attribute}=#{parts.pop}"
+      parts.reverse.each do |folder|
         dn += ",ou=#{folder}"
       end
       dn += ",#{@base_dn}"
@@ -374,7 +569,7 @@ plugin_class = Class.new do
       raise("Invalid 'ldap_uri' configuration: #{ldap_uri}")
     end
 
-    if config['base_dn']
+    if config.key?('base_dn')
       @base_dn = config['base_dn']
     else
       @base_dn = 'ou=simpkv,o=puppet,dc=simp'
@@ -382,7 +577,7 @@ plugin_class = Class.new do
     end
 
     admin_dn = nil
-    if config['admin_dn']
+    if config.key?('admin_dn')
       admin_dn = config['admin_dn']
     else
       #FIXME should not use admin for whole tree
@@ -435,6 +630,13 @@ plugin_class = Class.new do
       # unencrypted ldap or ldapi
       @cmd_env = ''
       @base_opts = "-x -D #{admin_dn} -y #{admin_pw_file} -H #{@ldap_uri}"
+    end
+
+    if config.key?('retries')
+      @retries = config['retries']
+    else
+      @retries = 1
+      Puppet.debug("simpkv plugin #{name}: Using retries = #{@retries}")
     end
   end
 
